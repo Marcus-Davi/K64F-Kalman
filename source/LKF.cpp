b65a5c6a25e51dc0dbd54a8909c03180dd7f8930
Marcus Davi <davi2812@dee.ufc.br>
2020-02-21 10:52:09 -0300

    LKF prototipo

---
+++ source/LKF.cpp
@@ -0,0 +1,21 @@
+/*
+ * LKF.cpp
+ *
+ *  Created on: 21 de fev de 2020
+ *      Author: marcus
+ */
+
+#include <LKF.h>
+
+namespace Kalman {
+
+LKF::LKF() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LKF::~LKF() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Kalman */
